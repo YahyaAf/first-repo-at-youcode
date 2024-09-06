0301ed576db025fc94d863d66776132c9fc6a6b3
Yahya <yahyaafadisse92@gmail.com>
2024-09-06 17:59:04 +0200

    add dossier mini project in fixer challenge fibonacci

--- challenges-fonctions/fonctions.c
+++ challenges-fonctions/fonctions.c
@@ -181,36 +181,23 @@ int main()
 challenge 6:
 
 
-int fobinacci(int n ){
-
-int i, f_n, f_0 = 0, f_1 = 1;
-
-  for (i = 0; i <= n; i++)
-  {
-    if (i <= 1)
-     f_n=i;
-    else
-    {
-      f_n = f_0 + f_1;
-      f_0 = f_1;
-     f_1 = f_n;
+int fib(int N){
+    if(N<2){
+        return N;
+    }else{
+        return fib(N-1)+fib(N-2);
     }
 
- } return printf( )f_n;
-
-
 }
-
 int main()
 {
-    int n ,fn ;
-
-  printf("Entrez le nombre de termes:");
-  scanf("%d", &n);
-  fn=fobinacci(n);
-
-  printf("U%d=%d \n ",n,fn);
-
+    int N,i;
+    printf("Donner le nombre N : ");
+    scanf("%d",&N);
+    printf("list de fibonacci de nombre N est : %d \n",N);
+    for(i=0;i<=N;i++){
+        printf("%d \n",fib(i));
+    }
 
     return 0;
 }
