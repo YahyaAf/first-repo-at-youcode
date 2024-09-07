b66294c5c33dc1a809d898a7fcf59902eb9ec8dd
Yahya afadisse <yahyaafadisse92@gmail.com>
2024-09-07 20:03:57 +0100

    add la fonction d'affichage pour le mini projet

--- Mini Projet/miniProjet.c
+++ Mini Projet/miniProjet.c
@@ -22,18 +22,33 @@ for(i=0;i<n;i++){
     scanf("%f",&liv[i].prix);
     printf("Donner la quantite de livre %d : ",i+1);
     scanf("%d",&liv[i].quantite);
+    printf("=====================\n");
+}
 }
 
+void Affiche(Livre liv[],int n){
+int i;
+
+for(i=0;i<n;i++){
+    printf("Livre %d : ",i+1);
+    printf("le titre de livre : %s \n",liv[i].titre);
+    printf("l'auteur de livre : %s \n",liv[i].auteur);
+    printf("le prix de livre  : %.2f \n",liv[i].prix);
+    printf("la quantite de livre : %d \n",liv[i].quantite);
+    printf("=====================\n");
 }
+}
+
 int main(){
-
 int n;
 printf("Donner le nombre de livre pour la creation : ");
 scanf("%d",&n);
 Livre liv[n];
 Ajouter(liv,n);
+Affiche(liv,n);
+
 
 
 return 0;
-}
 
+}
