ea139bdf1d3ee0e042075b4e9eeebc39aaa11915
Yahya afadisse <yahyaafadisse92@gmail.com>
2024-09-07 23:58:20 +0100

    add fonction delete and modifier fonction main

--- Mini Projet/miniProjet.c
+++ Mini Projet/miniProjet.c
@@ -10,10 +10,12 @@ float prix;
 int quantite;
 }Livre;
 
-void Ajouter(Livre liv[],int n){
+void Ajouter(Livre liv[],int *n){
 int i;
-
-for(i=0;i<n;i++){
+int count;
+printf("Donner le nombre de livre pour ajouter : ");
+scanf("%d",&count);
+for(i=0;i<*n+count;i++){
     printf("Donner le titre de livre %d : ",i+1);
     scanf("%s",liv[i].titre);
     printf("Donner l'auteur de livre %d : ",i+1);
@@ -24,6 +26,7 @@ for(i=0;i<n;i++){
     scanf("%d",&liv[i].quantite);
     printf("=====================\n");
 }
+*n=count+*n;
 }
 
 void Affiche(Livre liv[],int n){
@@ -53,23 +56,69 @@ void Update(Livre liv[],int n){
         liv[i].quantite=quantiteNew;
         trouve=1;
     }
+    }
     if(!trouve){
-        printf("le titre tu donner <%s> not found ",titreR);
+        printf("le titre tu donner <%s> not found \n",titreR);
     }
 }
+
+void Delete(Livre liv[],int *n){
+    char titreR[100];
+    int trouve=0;
+    printf("Donner le titre de livre pour la supression : ");
+    scanf("%s",titreR);
+    for(int i=0;i<*n;i++){
+    if(strcmp(liv[i].titre,titreR)==0){
+        printf("la supression valider \n"
+               );
+        trouve=1;
+        for(int j=i;j<*n-1;j++){
+            liv[j]=liv[j+1];
+        }
+        (*n)--;
+    }
+
+   }
+   if(!trouve){
+        printf("le titre tu donner <%s> not found \n",titreR);
 }
+}
+
 int main(){
-int n;
-printf("Donner le nombre de livre pour la creation : ");
-scanf("%d",&n);
-Livre liv[n];
-Ajouter(liv,n);
-Affiche(liv,n);
+    Livre liv[100];
+    int n = 0;
+    int choix;
 
-Update(liv,n);
-Affiche(liv,n);
+    do {
+        printf("\nMenu :\n");
+        printf("1- Ajouter des livres \n ");
+        printf("2- Afficher les livres \n ");
+        printf("3- Mettre à jour la quantité d'un livre \n ");
+        printf("4- Supprimer un livre \n ");
+        printf("5- Quitter\n");
+        printf("choisie un nombre from menu : ");
+        scanf("%d", &choix);
 
-
+        switch (choix) {
+            case 1:
+                Ajouter(liv, &n);
+                break;
+            case 2:
+                Affiche(liv, n);
+                break;
+            case 3:
+                Update(liv, n);
+                break;
+            case 4:
+                Delete(liv, &n);
+                break;
+            case 5:
+                printf("Quitter le programme \n");
+                break;
+            default:
+                printf("The number u chose not found \n");
+        }
+    } while (choix != 5);
 
 return 0;
 
